0e424d29dc33f2b474ac17e7ba1d33c89c53864a
kolach <chikolad@gmail.com>
2023-05-25 11:46:48 -0500

    add gitlink

--- lua/plugins/vcs/init.lua
+++ lua/plugins/vcs/init.lua
@@ -111,4 +111,17 @@ return {
       vim.g.gist_open_browser_after_post = 1
     end,
   },
+  {
+    "ruifm/gitlinker.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gy", "<cmd>lua require'gitlinker'.get_buf_range_url('n', {action_callback = require'gitlinker.actions'.open_in_browser})<cr>", desc = "Link" },
+      {
+        "<leader>gy",
+        "<cmd>lua require'gitlinker'.get_buf_range_url('v', {action_callback = require'gitlinker.actions'.open_in_browser})<cr>",
+        desc = "Link",
+        mode = "v",
+      },
+    },
+  },
 }
